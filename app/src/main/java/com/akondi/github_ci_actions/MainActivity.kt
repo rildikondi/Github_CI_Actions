effb4dabf743d913d388fd034e69b9361519ebb9
Amarildo <kondirildi@yahoo.com>
2020-10-02 15:36:10 +0200

    Initial commit

---
+++ app/src/main/java/com/akondi/github_ci_actions/MainActivity.kt
@@ -0,0 +1,11 @@
+package com.akondi.github_ci_actions
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
